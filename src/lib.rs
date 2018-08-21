5e9320c9187dd80877d095b98818691ca46a5e3f
matthieu <matthieu.gouel@gmail.com>
2018-08-21 23:43:00 +0200

    Move logic to a lib

---
+++ src/lib.rs
@@ -0,0 +1,33 @@
+//!
+//! Interpreter for `bjørn` language.
+//!
+
+mod token;
+mod lexer;
+mod parser;
+mod ast;
+mod interpreter;
+mod value;
+
+use lexer::Lexer;
+use parser::Parser;
+use interpreter::Interpreter;
+
+///
+/// Entrypoint of `bjorn` library.
+///
+/// ```
+/// extern crate bjorn;
+///
+/// let input = "2 + 2";
+/// println!("{}", bjorn::interpret(input));
+/// ```
+///
+pub fn interpret(input: &str) -> String {
+    Interpreter::new(
+        Parser::new(
+            Lexer::new(input)
+        )
+    ).interpret().to_string()
+}
+
--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,20 @@
+extern crate bjorn;
+
 use std::env;
+use std::io;
 use std::fs;
 
-pub mod token;
-pub mod lexer;
-pub mod parser;
-pub mod ast;
-pub mod interpreter;
-pub mod value;
-
-fn main() {
+fn main() -> io::Result<()> {
     let args: Vec<String> = env::args().collect();
     if args.len() < 2 {
-        println!("Please enter the file path.");
-        return;
+        return Err(
+            io::Error::new(io::ErrorKind::InvalidInput, "Please enter the file path.")
+        );
     }
 
-    let input = fs::read_to_string(&args[1])
-        .expect("Impossible to read the file.");
+    let input = fs::read_to_string(&args[1])?;
 
-    let _lexer = lexer::Lexer::new(input.as_str());
-    let _parser = parser::Parser::new(_lexer);
-    let mut _interpreter = interpreter::Interpreter::new(_parser);
+    println!("{}", bjorn::interpret(&input));
+    Ok(())
 
-    println!("{}", _interpreter.interpret());
 }
