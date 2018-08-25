792e51fb9a6d1368df7be183ccc30bc3208b4c72
matthieu <matthieu.gouel@gmail.com>
2018-08-25 17:16:03 +0200

    Add `scan` and `parse` functions to the lib

--- src/lib.rs
+++ src/lib.rs
@@ -14,6 +14,43 @@ use parser::Parser;
 use interpreter::Interpreter;
 
 ///
+/// Only proceed to the lexical analysis.
+/// For testing purposes.
+///
+/// ```
+/// extern crate bjorn;
+///
+/// let input = "2 + 2";
+/// println!("{:?}", bjorn::scan(input));
+/// ```
+///
+pub fn scan(input: &str) -> Vec<token::Token> {
+    let lexer = Lexer::new(input);
+    let mut scan = Vec::new();
+    for t in lexer {
+        scan.push(t);
+    }
+    scan
+}
+
+///
+/// Only proceed to the lexical and syntaxic analysis.
+/// For testing purposes.
+///
+/// ```
+/// extern crate bjorn;
+///
+/// let input = "2 + 2";
+/// println!("{:?}", bjorn::parse(input));
+/// ```
+///
+pub fn parse(input: &str) -> ast::AST {
+    Parser::new(
+        Lexer::new(input)
+    ).parse()
+}
+
+///
 /// Entrypoint of `bjorn` library.
 ///
 /// ```
